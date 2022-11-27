a9a10c78f0030ef472b6fdc3b3ed536e4c924ac6
Adam <am1macdonald@gmail.com>
2022-11-27 18:14:45 -0500

    Set up styles & remove boilerplate

---
+++ src/pages/home/login.tsx
@@ -0,0 +1,33 @@
+import styles from "./login.module.scss";
+
+interface props {
+  hello: string | undefined;
+}
+
+const Login = ({ hello }: props) => {
+  return (
+    <>
+      <div>
+        <h1>Brewtal</h1>
+        <p className={styles.subText}>
+          <strike>Stop</strike>
+          <span className={styles.caretSpacer}>^</span>the grind.
+        </p>
+        <p className={styles.slanted}>Love</p>
+      </div>
+      <div className="form-container">
+        <p>
+          Sign-up for our newsletter to stay up to date with development. `$
+          {hello}`
+        </p>
+        <form className={styles.form} id="newsletter-signup">
+          <label htmlFor="email">Email: </label>
+          <input type="email" name="email" id="email" />
+          <input className="btn" type="submit" value="enter"></input>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-montagu)', ...fontFamily.sans],
+        mono: ['var(--font-overpass)',...fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 };
