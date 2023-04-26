847e05cd3b557250a666f59425d1ad878c85c956
Adam MacDonald <am1macdonald@gmail.com>
2023-04-25 21:40:20 -0400

    ditch email signup

--- src/lib/authService.ts
+++ src/lib/authService.ts
@@ -1,43 +1,47 @@
 import { supabase } from '$lib/supabaseClient';
 
 async function signupWithEmail(email: string, password: string) {
-	const { data, error } = await supabase.auth.signUp({
-		email,
-		password
-	});
-	if (data) {
-		console.log(data);
-	} else if (error) {
-		console.log(error);
-	}
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password
+  });
+  if (data) {
+    console.log(data);
+  } else if (error) {
+    console.log(error);
+  }
 }
 
 async function signInWithEmail(email: string, password: string) {
-	const { data, error } = await supabase.auth.signInWithPassword({
-		email,
-		password
-	});
-	if (data) {
-		console.log(data);
-	} else if (error) {
-		console.log(error);
-	}
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  });
+  if (data) {
+    console.log(data);
+  } else if (error) {
+    console.log(error);
+  }
 }
 
 async function signInWithGoogle() {
-	const { data, error } = await supabase.auth.signInWithOAuth({
-		provider: 'google'
-	});
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      redirectTo: 'http//:localhost:5173/welcome/'
+    }
 
-	if (data) {
-		console.log(data);
-	} else if (error) {
-		console.log(error);
-	}
+  });
+
+  if (data) {
+    console.log(data);
+  } else if (error) {
+    console.log(error);
+  }
 }
 
 async function signOut() {
-	const { error } = await supabase.auth.signOut();
+  const { error } = await supabase.auth.signOut();
 }
 
 export { signupWithEmail, signInWithEmail, signInWithGoogle, signOut };
--- src/routes/(app)/login/+page.svelte
+++ src/routes/(app)/login/+page.svelte
@@ -1,25 +1,13 @@
 <script>
 	import AppButton from '$lib/components/inputs/buttons/AppButton.svelte';
 	import Modal from '$lib/components/containers/Modal.svelte';
-
-	let loggedIn = false;
-
-	function handleSignOut() {
-		loggedIn = false;
-	}
-
-	function handleSignIn() {
-		loggedIn = true;
-	}
+	import { signInWithGoogle } from '$lib/authService';
 </script>
 
 <Modal>
 	<h3 class="mb-10 text-5xl">hey</h3>
 	<div class="flex flex-col items-center justify-center gap-1">
-			<span>SIGN IN</span>
-			<AppButton btnText={'Google'} on:message={handleSignIn} />
-			<AppButton btnText={'Email'} on:message={handleSignIn} />
+		<span>SIGN IN</span>
+		<AppButton btnText={'Google'} on:message={signInWithGoogle} />
 	</div>
-	<div><span>new?</span><span><a href="/signup">sign up.</a></span></div>
-	<span class="self-start">or don't...</span>
 </Modal>
