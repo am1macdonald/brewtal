ec9a96d6941eaaf26fdbb4546a21e676b77a2aed
Adam MacDonald <am1macdonald@gmail.com>
2023-05-07 16:42:23 -0400

    scaffold profile page

--- src/routes/(app)/profile/+layout.svelte
+++ src/routes/(app)/profile/+layout.svelte
@@ -1,7 +1,23 @@
-<script>
+<script lang="ts">
 	import AppSidePanel from '$lib/components/layout/AppSidePanel.svelte';
+
+	const links = [
+		{ text: 'Profile', route: '/profile' },
+		{ text: 'My Brews', route: '/profile/bruise' },
+		{ text: 'Settings', route: '/profile/settings' }
+	];
 </script>
 
 <AppSidePanel>
+	<div slot="sidePanel">
+		<h6>BREWTAL</h6>
+		<ul>
+			{#each links as { text, route }}
+				<li>
+					<a href={route}>{text}</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
 	<slot slot="viewport" />
 </AppSidePanel>
--- src/routes/(app)/profile/+page.svelte
+++ src/routes/(app)/profile/+page.svelte
@@ -1,3 +1,14 @@
-<div class="h-screen flex-1">
+<script>
+	import Container from '$lib/components/containers/Container.svelte';
+</script>
 
+<div class="h-screen flex-1">
+	<Container
+		><div>
+			<div class="bg-gray-400 h-36 w-24">
+				<img alt="profile pic" src="#" />
+			</div>
+			<div />
+		</div>
+	</Container>
 </div>
