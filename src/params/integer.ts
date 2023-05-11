21db118b65eec19bdfb932608c8994b0da567def
Adam MacDonald <am1macdonald@gmail.com>
2023-05-10 21:40:15 -0400

    param for route validation

---
+++ src/params/integer.ts
@@ -0,0 +1,12 @@
+import { redirect } from "@sveltejs/kit";
+
+/** @type {import('@sveltejs/kit').ParamMatcher} */
+export function match(param: unknown) {
+  if (typeof param === 'number') {
+    return /^\d+$/.test(param.toString());
+  } else if (typeof param === 'string') {
+    return /^\d+$/.test(param);
+  } else {
+    throw redirect(300, '/')
+  }
+}
