e2cf5e12516ebd352e235f149f5041c9b5a99db0
Adam MacDonald <am1macdonald@gmail.com>
2023-05-10 21:42:07 -0400

    add margin

--- src/lib/components/containers/Container.svelte
+++ src/lib/components/containers/Container.svelte
@@ -1,3 +1,3 @@
-<div class="flex justify-center items-center p-2.5 box-border bg-gray-900 rounded-md">
+<div class="flex justify-center items-center p-2.5 box-border bg-gray-900 rounded-md m-2">
 	<slot />
 </div>
