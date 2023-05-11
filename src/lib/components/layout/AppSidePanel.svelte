2304b975e238f661ffcd65d9d7fc4642cd4eb7f8
Adam MacDonald <am1macdonald@gmail.com>
2023-05-10 21:41:55 -0400

    tweak layout

--- src/lib/components/layout/AppSidePanel.svelte
+++ src/lib/components/layout/AppSidePanel.svelte
@@ -1,5 +1,5 @@
 <div class="flex h-screen w-screen">
-	<div class="h-screen bg-amber-300 w-80">
+	<div class="h-screen bg-amber-300 w-80 p-2.5">
 		<slot name="sidePanel" />
 	</div>
 	<slot name="viewport" />
--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -2,5 +2,4 @@
 	class="fixed bottom-0 left-0 flex h-10 w-screen flex-row items-center justify-center gap-10 bg-gray-800 text-amber-50"
 >
 	<span>BREWTAL</span>
-	<span>This is a footer element</span>
 </footer>
