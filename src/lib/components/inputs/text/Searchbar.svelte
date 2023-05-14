2546d8f9f67d30d31ae37306e1c8e1db1b5ad0dd
Adam MacDonald <am1macdonald@gmail.com>
2023-05-13 22:41:33 -0400

    resolve errors

--- src/lib/components/inputs/text/Searchbar.svelte
+++ src/lib/components/inputs/text/Searchbar.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
   import type { SuggestionProperty } from "$lib/types/types";
   export let suggestions: SuggestionProperty [];
+  function fireEvent(val: unknown) {
+
+    console.log(val);
+  }
 </script>
 
 <input class="flex justify-between items-center w-96" type="text">
 <div>
   <ul>
     {#each suggestions as s }
-    <li></li>
+    <li on:click={() => fireEvent(s.value)} >
+        {#if s.url !== ''}
+          <a href="{s.url}">{s.content}</a>
+        {/if}
+      </li>
     {/each}
   </ul>
 </div>
