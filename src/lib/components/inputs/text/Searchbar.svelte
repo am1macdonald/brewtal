79b01ed9c59bce8a1549f544d89df62ca8c501b4
Adam MacDonald <am1macdonald@gmail.com>
2023-05-14 17:42:30 -0400

    update style

--- src/lib/components/inputs/text/Searchbar.svelte
+++ src/lib/components/inputs/text/Searchbar.svelte
@@ -8,7 +8,15 @@
 </script>
 
 <div class="flex flex-col justify-start items-center w-fit">
-	<input bind:value class="flex justify-between items-center w-96" type="text" />
+	<label class="border-2 border-solid rounded border-gray-70 w-fit flex flex-row justify-between items-center" for="search">
+		<img class="invert h-12 w-12" src="/icons/icon-search.svg" alt="search" />
+		<input
+			bind:value
+			name="search"
+			class="flex justify-between items-center w-96 border-none"
+			type="text"
+		/>
+	</label>
 	<div>
 		<ul>
 			{#each suggestions as s}
