d9042777ff9a63a82ebaf9225654a57ff4f474b2
Adam MacDonald <am1macdonald@gmail.com>
2023-05-14 17:08:02 -0400

    wrap with div

--- src/lib/components/inputs/text/Searchbar.svelte
+++ src/lib/components/inputs/text/Searchbar.svelte
@@ -1,21 +1,24 @@
 <script lang="ts">
-  import type { SuggestionProperty } from "$lib/types/types";
-  export let suggestions: SuggestionProperty [];
-  function fireEvent(val: unknown) {
-
-    console.log(val);
-  }
+	import type { SuggestionProperty } from '$lib/types/types';
+	export let suggestions: SuggestionProperty[];
+	function fireEvent(val: unknown) {
+		console.log(val);
+	}
 </script>
 
-<input class="flex justify-between items-center w-96" type="text">
 <div>
-  <ul>
-    {#each suggestions as s }
-    <li on:click={() => fireEvent(s.value)} >
-        {#if s.url !== ''}
-          <a href="{s.url}">{s.content}</a>
-        {/if}
-      </li>
-    {/each}
-  </ul>
+	<input class="flex justify-between items-center w-96" type="text" />
+	<div>
+		<ul>
+			{#each suggestions as s}
+				<li>
+					<button on:click={() => fireEvent(s.value)}>
+						{#if s.url !== ''}
+							<a href={s.url}>{s.content}</a>
+						{/if}
+					</button>
+				</li>
+			{/each}
+		</ul>
+	</div>
 </div>
