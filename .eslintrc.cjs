8025e898f3ba1d0dd5bc1aecde680ff6a44f2553
Adam MacDonald <am1macdonald@gmail.com>
2023-06-27 20:47:12 -0400

    Fix eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,23 @@
 module.exports = {
-	root: true,
-	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
+	// ...
+	extends: ["plugin:svelte/recommended"],
+	// ...
+	parser: "@typescript-eslint/parser",
 	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
+		// ...
+		project: "tsconfig.json",
+		extraFileExtensions: [".svelte"], // This is a required setting in `@typescript-eslint/parser` v4.24.0.
 	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	}
-};
+	overrides: [
+		{
+			files: ["*.svelte"],
+			parser: "svelte-eslint-parser",
+			// Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
+			parserOptions: {
+				parser: "@typescript-eslint/parser",
+			},
+		},
+		// ...
+	],
+	// ...
+}
