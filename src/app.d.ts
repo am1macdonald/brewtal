2a3f80812d8ee99946888accff210bde02eef7c3
Adam MacDonald <am1macdonald@gmail.com>
2023-08-24 21:31:05 -0400

    Update src files

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,20 +1,13 @@
 // See https://kit.svelte.dev/docs/types#app
 
-import type { SupabaseClient } from "@supabase/supabase-js";
-
 // for information about these interfaces
 declare global {
-  namespace App {
-    // interface Error {}
-    interface Locals {
-      getSession: () => Promise<session | null>;
-      supabase: SupabaseClient<Database>;
-    }
-    interface PageData {
-      session: Session | null
-    }
-    // interface Platform {}
-  }
+	namespace App {
+		// interface Error {}
+		//interface Locals {}
+		//interface PageData {}
+		// interface Platform {}
+	}
 }
 
-export { };
+export {};
